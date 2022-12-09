6c2fc7d975b04e3bcec945d7eada5886bd22d685
yitian.wang <yitian.wang@okg.com>
2022-12-10 02:13:44 +0800

    fix blogs show

---
+++ src/pages/blogs/blog/components/header.jsx
@@ -0,0 +1,15 @@
+
+export default function BlogHeader () {
+  return (
+    <header className="site-header">
+      <div className="wrapper">
+        <a className="site-title" href="/">Eten Wang</a>
+        <nav className="site-nav">
+          {/* <a href="#" className="menu-icon"></a> */}
+          <div className="trigger">
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
+}
