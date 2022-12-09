24f6f48dda7d6cf87d1d1af84017a4ce93a8aef1
yitian.wang <yitian.wang@okg.com>
2022-12-10 00:56:19 +0800

    switch gastby to astro

--- src/helper/image.js
+++
@@ -1,4 +0,0 @@
-export const aliyun = (year, name) => {
-    const encodeName = encodeURIComponent(name)
-    return `https://eten-wang.oss-cn-beijing.aliyuncs.com/games/${year}/${encodeName}.jpg?x-oss-process=style/game.eten.wang`
-}
