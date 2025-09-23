8858c0a79ed0ccd434f04f310d2959246842a607
dandananddada <dandananddada@163.com>
2025-09-24 03:46:30 +0800

    sync my introduction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [yaml()]
+    plugins: [yaml(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      }
+    },
+    build: {
+      sourcemap: true, // 确保 sourcemap 配置正确
+    },
+    server: {
+      sourcemap: true, // 确保开发服务器也启用 sourcemap
+    }
   },
   site: 'https://eten.wang',
 });
